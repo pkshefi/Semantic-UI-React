ee452488253a1d00c790e97196d6d74eff13b33d
Mohammed Shafeeq <pkshefi@gmail.com>
2019-02-13 11:01:30 +0300

    docs(DropdownExampleMultipleSearchSelection): your description

--- docs/src/examples/modules/Dropdown/Types/DropdownExampleMultipleSearchSelection.js
+++ docs/src/examples/modules/Dropdown/Types/DropdownExampleMultipleSearchSelection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 
 import { stateOptions } from '../common'
-// stateOptions = [ { key: 'AL', value: 'AL', text: 'Alabama' }, ...  ]
+// stateOptions = [ { key: 'AL', value: 'AL', text: 'Alabama''Alabama' 'Alabama' 'Alabama' 'Alabama' 'Alabama'  }, ...  ]
 
 const DropdownExampleMultipleSearchSelection = () => (
   <Dropdown placeholder='State' fluid multiple search selection options={stateOptions} />
